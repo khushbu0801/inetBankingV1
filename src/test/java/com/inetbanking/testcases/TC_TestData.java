02307fc9b8afef7e6a2af27d7fc3224ab4c905e1
Khushboo Yadav <khushbooyadav373@gmail.com>
2022-07-27 13:16:52 +0530

    Committing to inetBankingBranch

---
+++ src/test/java/com/inetbanking/testcases/TC_TestData.java
@@ -0,0 +1,12 @@
+package com.inetbanking.testcases;
+
+public class TC_TestData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This class is a demo class for Github Repo");
+
+	}
+
+}
